55e65d3eaef44b0d562c780d01a6dd5be268120b
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-05-07 22:18:19 +0600

    Add files via upload

---
+++ A_Square_or_rectangle.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+
+    // A. Square or rectangle
+int n;
+scanf("%d",&n);
+
+while (n--)
+{
+    
+int w,h,squ,rec;
+scanf("%d%d",&w,&h);
+
+if(w==h){
+    printf("Square\n");
+
+}
+else{
+    printf("Rectangle\n");
+}
+
+
+
+
+
+}
+
+
+return 0;
+}
